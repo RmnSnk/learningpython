ccf0ed4dde74348ce442e3373c48f4a77b64925b
Romain Sonneck <romain.sonneck@gmail.com>
2020-03-15 21:52:46 +0100

    mis a jour du fichier volBoite qui ne marche pas

--- volBoite.py
+++ volBoite.py
@@ -1,11 +1,33 @@
-def volBoite(a, b, c):
+
+# prg buggé
+
+valeur = []
+
+
+def volBoite(a=10, b=10, c=10):
     return a * b * c
     
     
-a = int(input('Merci de saisir la prenmière cote : '))
-b = int(input('Merci de saisir la deuxième cote : '))
-c = int(input('Merci de saisir la troisième cote : '))
+def saisie():
+    global valeur
+    for i in range(3):
+        print('Entrez la valeur ' + str(i + 1) + " : ", end='')
+        v = input()
+        if v == "":
+            valeur.append(v)
+        else:
+            valeur.append(int(v))
+        i = + 1
+    return valeur
 
 
+saisie()
+print(valeur)
+
+a = valeur[0]
+b = valeur[1]
+c = valeur[2]
 
 print(volBoite(a, b, c))
+
+
