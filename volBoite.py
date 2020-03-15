e9f00786eb417702cc4fe10a55b3121bb8f6e204
Romain Sonneck <romain.sonneck@gmail.com>
2020-03-15 21:11:26 +0100

    mis a jour la branche test

---
+++ volBoite.py
@@ -0,0 +1,11 @@
+def volBoite(a, b, c):
+    return a * b * c
+    
+    
+a = int(input('Merci de saisir la prenmière cote : '))
+b = int(input('Merci de saisir la deuxième cote : '))
+c = int(input('Merci de saisir la troisième cote : '))
+
+
+
+print(volBoite(a, b, c))
