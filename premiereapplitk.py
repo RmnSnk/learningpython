d6e1d45efcabd19a0e683bb06bff54286d356392
Romain Sonneck <romain.sonneck@gmail.com>
2020-03-23 02:33:49 +0100

    ajout de gitignore

--- premiereapplitk.py
+++ premiereapplitk.py
@@ -8,8 +8,14 @@ def drawline():
     "Tracé d'une ligne dans le canevas can1"
     global x1, y1, x2, y2, coul
     can1.create_line(x1, y1, x2, y2, width=1, fill=coul)
-    # modification des coordonnées pour la ligne suivante :
-    y2, y1 = y2 + 10, y1 - 10
+    # modification des coordonnées pour la ligne suivante:
+    y2, y1 = y2 + 10, y1 + 10
+
+def drawline2():
+    "Trace une croix rouge au centre"
+    coul = 'red'
+    can1.create_line(0, 250, 650, 250, width=1, fill=coul)
+    can1.create_line(325, 0, 325, 500, width=1, fill=coul)
 
 def changecolor():
     "Changement aléatoire de la couleur du tracé"
@@ -23,12 +29,12 @@ def changecolor():
 
 
 # les variables suivantes seront utilisées de manière globale :
-x1, y1, x2, y2 = 10, 190, 190, 10 # coordonnées de la ligne
+x1, y1, x2, y2 = 0, 10, 650, 10  # coordonnées de la ligne
 coul = 'dark green' # couleur de la ligne
 # Création du widget principal ("maître") :
 fen1 = Tk()
 # création des widgets "esclaves" :
-can1 = Canvas(fen1, bg='dark grey', height=200, width=200)
+can1 = Canvas(fen1, bg='dark grey', height=500, width=650)
 can1.pack(side=LEFT)
 bou1 = Button(fen1,text='Quitter',command=fen1.quit)
 bou1.pack(side=BOTTOM)
@@ -36,5 +42,7 @@ bou2 = Button(fen1,text='Tracer une ligne',command=drawline)
 bou2.pack()
 bou3 = Button(fen1,text='Autre couleur',command=changecolor)
 bou3.pack()
-fen1.mainloop() # démarrage du réceptionnaire d'événements
+bou4 = Button(fen1, text='Trace une croix rouge', command=drawline2)
+bou4.pack()
+fen1.mainloop()  # démarrage du réceptionnaire d'événements
 fen1.destroy()  # destruction (fermeture) de la fenêtre
