b5ff66c23cc67ee861e0992d57f8ce1fe29b06df
Romain Sonneck <romain.sonneck@gmail.com>
2020-03-25 00:41:06 +0100

    damier ok

--- damier.py
+++ damier.py
@@ -6,12 +6,24 @@ from tkinter import *
 # on boucle la fonction pour pour faire toutes les lignes
 
 def damier():
-    x, y = 0, 0
+    'Fonction qui créé un damier'
+    x, y, j = 0, 0, 0
     col1, col2 = 'ivory', 'black'
-    for i in range(10):
-        can.create_rectangle(x, y, x + 100, y + 100, fill=col1)
-        x += 100
-        col1, col2 = col2, col1
+    for j in range(10):
+        for i in range(10):
+            can.create_rectangle(x, y, x + 100, y + 100, fill=col1)
+            x += 100
+            col1, col2 = col2, col1
+            i += 1
+        y += 100
+        x = 0
+        col1, col2 = col2, col1  # Pour changer de couleur au début d'une nouvelle ligne
+        j += 1
+
+def point():
+    'Fonction qui place un point au hasard'
+    pass
+
 
 
 
