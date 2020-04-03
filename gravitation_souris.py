24af2e5d68a995e89176b18eed78fc88eb6b614e
Romain Sonneck <romain.sonneck@gmail.com>
2020-04-03 16:06:33 +0200

    travail sur gravitation_souris.py

--- gravitation_souris.py
+++ gravitation_souris.py
@@ -11,20 +11,25 @@ def choix_planete():
     global planete
     planete = planete_choisie.get()
     choix.configure(text='La planete choisie est : ' + str(planete))
+    return planete
     
 def pointeur(event):
-    'Fonction qui donne les coordonnées du clic'
+    'Fonction qui déplace la planete'
     clicx = event.x
     clicy = event.y
-    print (clicx)  # debug
-    print (clicy)
-    return (clicx, clicy)
-
-
-
-def creation_planete():
-    'Créer une planete ou l\' n on clique'
+    global mars, terre
+    if choix_planete() == 'Mars':
+        can1.coords(oval_mars, clicx - mars[3], clicy - mars[3], clicx + mars[3], clicy + mars[3])
+    else:
+        can1.coords(oval_terre, clicx - terre[3], clicy - terre[3], clicx + terre[3], clicy + terre[3])
     
+    # return (clicx, clicy)
+    
+    
+# Liste représentant les planetes [xdépart, ydépart, masse en kg, rayon en px, couleur]
+mars = [300, 500, 6.39 * 10**23, 20, 'red']
+terre = [700, 500, 5.972 * 10**24, 38, 'blue']
+
 
 # Création du widget principal
 fen1 = Tk()
@@ -34,6 +39,10 @@ fen1.title('Test buttons radio')
 can1 = Canvas(fen1, bg='black', height=1000, width=1000)
 can1.grid(row=2, column=1, columnspan=6)
 can1.bind("<Button-1>", pointeur)
+
+# Création de planetes en position départ
+oval_mars = can1.create_oval(mars[0] - mars[3], mars[1] - mars[3], mars[0] + mars[3], mars[1] + mars[3], fill=mars[4])
+oval_terre = can1.create_oval(terre[0] - terre[3], terre[1] - terre[3], terre[0] + terre[3], terre[1] + terre[3], fill=terre[4])
 
 # Création de widget esclaves
 
