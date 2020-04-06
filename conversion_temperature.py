87b31ea680475fda80a61b842bd8ba036eeb0b48
Romain Sonneck <romain.sonneck@gmail.com>
2020-04-06 14:18:55 +0200

    Finalisation de conversion_temperature.py

--- conversion_temperature.py
+++ conversion_temperature.py
@@ -9,27 +9,37 @@ from tkinter import *
 
 #----------- Fonctions
 
-def farenheit_vers_celcius():
+def farenheit_vers_celcius(event):
     'fonction qui transforme des farenheit en celcius'
-    pass
+    f = int(entree_f.get())
+    c = (f - 32) / 1.8
+    varTC.set(str(c))
 
-def celcius_vers_farenheit():
+
+def celcius_vers_farenheit(event):
     'fonction qui transforme des celcius en farenheit'
-    pass
-    
+    c = int(entree_c.get())
+    f=c * 1.8 + 32
+    varTF.set(str(f))
+
 
 # ------------Widgets
-fen = Tk()
+fen=Tk()
 fen.title('Conversion °F < - > °C')
+
+varTF=StringVar()
+varTC=StringVar()
 
 # Label de textes et champs de saisie
 Label(fen, text='Température en °F : ').grid(row=1, column=1)
-entree_f = Entry(fen)
+entree_f=Entry(fen, textvariable=varTF)
+entree_f.bind("<Return>", farenheit_vers_celcius)
 entree_f.grid(row=1, column=2)
 
 
 Label(fen, text='Température en °C : ').grid(row=2, column=1)
-entree_c = Entry(fen)
+entree_c=Entry(fen, textvariable=varTC)
+entree_c.bind("<Return>", celcius_vers_farenheit)
 entree_c.grid(row=2, column=2)
 
 
