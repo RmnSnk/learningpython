7daeb16677fa73012f1d7d01ebfa02ee96880ce3
Romain Sonneck <romain.sonneck@gmail.com>
2020-04-28 14:34:58 +0200

    travail sur exofichier1.py

--- manipulation_f/exofichier1.py
+++ manipulation_f/exofichier1.py
@@ -3,65 +3,93 @@
 # Premier programme de manipulation de fichiers
 
 
-
-
 def fichier():
     global nom_fichier
     nom_fichier = input('Merci de saisir un nom de fichier # : ')
+    print('\n')
     
 
 def existance_fichier():
-    'Fonction qui vérifié que le fichier existe'
+    'Fonction qui vérifié que le fichier existe ou le crée'
     global nom_fichier
     try:
         f = open(nom_fichier, "r")
         f.close()
-        return 1
+        print('Ouverture du fichier ' + str(nom_fichier) + '.')
     except:
-        fichier()
-        return 0
+        f = open(nom_fichier, "w")
+        f.close()
+        print('Le fichier ' + str(nom_fichier) + ' a été créé.')
 
 def choix_action():
+    print()
+    print('Fichier de travail :' + str(nom_fichier) + '\n')
     print('Que souhaitez vous faire ?')
-    print('    Afficher un fichier :    1')
-    print('    Ecrire dans le fichier : 2 \n')
+    print('    Choisir un fichier :       1')
+    print('    Afficher le fichier :      2')
+    print('    Ecrire dans le fichier :   3')
+    print('    Quittez :                  4 \n')
     while 1:
         try:
-            choix = int(input('Saisir 1 ou 2 : '))
-            if choix == 1 or choix == 2:
+            choix = int(input('Saisir 1, 2, 3 ou 4 : \n'))
+            if choix == 1 or choix == 2 or choix == 3 or choix == 4:
                 break
             else:
                 print()
-                print('On a  dit 1 OU 2 ! \n', color='red')
+                print('On a  dit 1, 2, 3 ou 4 ! \n', color='red')
                 choix_action()
         except:
-            print('On a dit 1 OU 2 ! \n')
+            print('On a dit 1,2,3 ou 4 ! \n')
             print()
             choix_action()
             
-    if choix == 1:
-        print('Vous avez choisi 1')
+    if choix == 2:
         lire_fichier()
-    else:
-        print('Vous avez choisi 2')
+        print()
+        choix_action()
+    elif choix == 3:
         ecrire_fichier()
-    
-def lire_fichier():  # a coder
-    'Fonction qui permet de lire un fichier'
-    pass
+        print()
+        choix_action()
+    elif choix == 1:
+        fichier()
+        existance_fichier()
+        choix_action()
+    else:
+        print('Exit ...')
 
-def ecrire_fichier():  # a coder
+def lire_fichier():
+    'Fonction qui permet de lire un fichier'
+    global nom_fichier
+    print('Affichage du fichier : \n' + str(nom_fichier))
+    try:
+        f = open(nom_fichier, 'r')
+        t = f.read()
+        print(t)
+        f.close()
+    except:
+        choix_action()
+
+def ecrire_fichier():  # a coder pour écrire ligne par ligne
     'Fonction qui ecrit dans un fichier'
-    pass
+    global nom_fichier
+    print('Ecriture dans le fichier :' + str(nom_fichier))
+    try:
+        f = open(nom_fichier, 'a')
+        i = 1
+        while i:
+            ligne = input("# : ")
+            if ligne == "":
+                i = 0
+            else:
+                f.write(ligne)
+    except:
+        choix_action()
 
 
 # programme principal
 
-fichier()
-
-while existance_fichier() == 0:
-    existance_fichier()
-
+nom_fichier = "Aucun, merci de d'abord choisir un fichier (1)"
 choix_action()
 
 
