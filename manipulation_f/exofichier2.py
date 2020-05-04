2d5c9e2e452a696d6e97261b55674a30832424b7
Romain Sonneck <romain.sonneck@gmail.com>
2020-05-04 15:39:27 +0200

    travail sur exofichier2

--- manipulation_f/exofichier2.py
+++ manipulation_f/exofichier2.py
@@ -28,18 +28,19 @@ def choix_action():
     print('    Choisir un fichier :       1')
     print('    Afficher le fichier :      2')
     print('    Ecrire dans le fichier :   3')
-    print('    Quittez :                  4 \n')
+    print('    Phrase la plus longue :    4')
+    print('    Quittez :                  5 \n')
     while 1:
         try:
-            choix = int(input('Saisir 1, 2, 3 ou 4 : \n'))
-            if choix == 1 or choix == 2 or choix == 3 or choix == 4:
+            choix = int(input('Saisir 1, 2, 3, 4 ou 5 : \n'))
+            if choix == 1 or choix == 2 or choix == 3 or choix == 4 or choix == 5:
                 break
             else:
                 print()
-                print('On a  dit 1, 2, 3 ou 4 ! \n', color='red')
+                print('On a  dit 1, 2, 3, 4 ou 5 ! \n', color='red')
                 choix_action()
         except:
-            print('On a dit 1,2,3 ou 4 ! \n')
+            print('On a dit 1,2,3,4 ou 5 ! \n')
             print()
             choix_action()
             
@@ -55,8 +56,12 @@ def choix_action():
         fichier()
         existance_fichier()
         choix_action()
+    elif choix == 4:
+        phrase_longue()
+        choix_action()
     else:
         print('Exit ...')
+        
 
 def lire_fichier():
     'Fonction qui permet de lire un fichier'
@@ -84,8 +89,30 @@ def ecrire_fichier():  # a coder pour écrire ligne par ligne
             else:
                 f.write(ligne)
                 f.write('\n')
+                f.close()
     except:
         choix_action()
+
+def phrase_longue():
+    'Fonction qui trouve la phrase la plus longue'
+    global nom_fichier
+    print('### La phrase la plus longue du fichier ' +
+          str(nom_fichier) + ' est : ###')
+    f = open(nom_fichier, 'r')
+    i = 1
+    tmax = 'Fichier vide'
+    nmax = 0
+    while i:
+        t = f.readline()
+        n = len(t)
+        if n > nmax:
+            nmax = n
+            tmax = t
+        if n == 0:
+            f.close()
+            print(tmax + "(" + str(nmax-1) + ' caractères)')
+            break
+        
 
 
 # programme principal
